461d2de783b4e6ea553d0d0ca0e35d77a3606606
Hitesh Sutar <hvishwassutar@deloitte.com>
2020-05-09 23:21:23 +0530

    Initial Commit

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Hitesh, Vishwas Sutar on 09/05/20.
+//  Copyright © 2020 Hitesh Sutar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
